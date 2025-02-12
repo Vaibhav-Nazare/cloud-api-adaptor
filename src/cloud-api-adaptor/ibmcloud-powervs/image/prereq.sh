e63559619c0daa14ba17ae5fca01cd6f43126940
Vaibhav-Nazare <87978158+Vaibhav-Nazare@users.noreply.github.com>
2025-02-12 13:01:04 +0530

    Update prereq.sh
    Signed-off-by: Vaibhav-Nazare <87978158+Vaibhav-Nazare@users.noreply.github.com>

--- src/cloud-api-adaptor/ibmcloud-powervs/image/prereq.sh
+++ src/cloud-api-adaptor/ibmcloud-powervs/image/prereq.sh
@@ -4,9 +4,15 @@
 GO_VERSION="1.22.12"
 
 # Install dependencies
-yum install -y curl libseccomp-devel openssl openssl-devel skopeo clang clang-devel protobuf-compiler device-mapper-devel
+yum install -y curl libseccomp-devel openssl openssl-devel skopeo clang clang-devel
+
+wget https://rpmfind.net/linux/centos-stream/9-stream/CRB/ppc64le/os/Packages/protobuf-compiler-3.14.0-13.el9.ppc64le.rpm
+yum install -y protobuf-compiler-3.14.0-13.el9.ppc64le.rpm
+
+wget https://www.rpmfind.net/linux/centos-stream/9-stream/CRB/ppc64le/os/Packages/device-mapper-devel-1.02.202-6.el9.ppc64le.rpm
+yum install -y device-mapper-devel-1.02.202-6.el9.ppc64le.rpm
 
 # Install Golang
-curl https://dl.google.com/go/go${GO_VERSION}.linux-ppc64le.tar.gz -o go${GO_VERSION}.linux-ppc64le.tar.gz && \
+curl https://go.dev/dl/go${GO_VERSION}.linux-ppc64le.tar.gz -o go${GO_VERSION}.linux-ppc64le.tar.gz && \
 rm -rf /usr/local/go && tar -C /usr/local -xzf go${GO_VERSION}.linux-ppc64le.tar.gz && \
 rm -f go${GO_VERSION}.linux-ppc64le.tar.gz
