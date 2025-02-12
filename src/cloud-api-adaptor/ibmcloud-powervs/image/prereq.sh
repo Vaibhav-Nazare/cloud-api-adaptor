6a9b4c710e0be0bc842cf21710d5a59bafafc9b5
Vaibhav-Nazare <87978158+Vaibhav-Nazare@users.noreply.github.com>
2025-02-12 10:28:05 +0530

    Update prereq.sh
    Install protobuf-compiler and device-mapper-devel using yum package manager
    
    Signed-off-by: Vaibhav-Nazare <87978158+Vaibhav-Nazare@users.noreply.github.com>

--- src/cloud-api-adaptor/ibmcloud-powervs/image/prereq.sh
+++ src/cloud-api-adaptor/ibmcloud-powervs/image/prereq.sh
@@ -4,13 +4,7 @@
 GO_VERSION="1.22.12"
 
 # Install dependencies
-yum install -y curl libseccomp-devel openssl openssl-devel skopeo clang clang-devel
-
-wget https://rpmfind.net/linux/centos-stream/9-stream/CRB/ppc64le/os/Packages/protobuf-compiler-3.14.0-13.el9.ppc64le.rpm
-yum install -y protobuf-compiler-3.14.0-13.el9.ppc64le.rpm
-
-wget https://www.rpmfind.net/linux/centos-stream/9-stream/CRB/ppc64le/os/Packages/device-mapper-devel-1.02.197-2.el9.ppc64le.rpm
-yum install -y device-mapper-devel-1.02.197-2.el9.ppc64le.rpm
+yum install -y curl libseccomp-devel openssl openssl-devel skopeo clang clang-devel protobuf-compiler device-mapper-devel
 
 # Install Golang
 curl https://dl.google.com/go/go${GO_VERSION}.linux-ppc64le.tar.gz -o go${GO_VERSION}.linux-ppc64le.tar.gz && \
